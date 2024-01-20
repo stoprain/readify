1dc2b0138ebfdfef8c437ac5492b44e46d39d0b9
stoprain <mystoprain@gmail.com>
2024-01-20 14:21:25 +0800

    in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tinyreader/screens/homepage_screen.dart';
 
 void main() {
@@ -20,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomeScreen(),
+      home: const MyHomeScreen(),
     );
   }
 }
--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -40,41 +40,71 @@ class SettingScreenState extends State<SettingScreen> {
       body: Center(
           child: Column(
         children: [
-          const Text('api address'),
-          StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-            _apiText.text = _api;
-            return TextField(
-              controller: _apiText,
-            );
-          }),
-          const Text('user'),
-          StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-            _userText.text = _user;
-            return TextField(
-              controller: _userText,
-            );
-          }),
-          const Text('pass'),
-          StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-            _passText.text = _pass;
-            return TextField(
-              controller: _passText,
-            );
-          }),
-          ElevatedButton(
-            onPressed: () {
-              Preference.setString(Preference.API, _apiText.text);
-              Preference.setString(Preference.USER, _userText.text);
-              Preference.setString(Preference.PASS, _passText.text);
-              if (context.mounted) {
-                Navigator.pop(context);
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'API address'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter API address';
               }
+              return null;
             },
-            child: const Text('Save'),
+            onSaved: (value) {
+              _api = value!;
+            },
           ),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'user'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter user';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'pass'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter pass';
+              }
+              return null;
+            },
+          ),
+          // const Text('api address'),
+          // StatefulBuilder(
+          //     builder: (BuildContext context, StateSetter setState) {
+          //   _apiText.text = _api;
+          //   return TextField(
+          //     controller: _apiText,
+          //   );
+          // }),
+          // const Text('user'),
+          // StatefulBuilder(
+          //     builder: (BuildContext context, StateSetter setState) {
+          //   _userText.text = _user;
+          //   return TextField(
+          //     controller: _userText,
+          //   );
+          // }),
+          // const Text('pass'),
+          // StatefulBuilder(
+          //     builder: (BuildContext context, StateSetter setState) {
+          //   _passText.text = _pass;
+          //   return TextField(
+          //     controller: _passText,
+          //   );
+          // }),
+          // ElevatedButton(
+          //   onPressed: () {
+          //     Preference.setString(Preference.API, _apiText.text);
+          //     Preference.setString(Preference.USER, _userText.text);
+          //     Preference.setString(Preference.PASS, _passText.text);
+          //     if (context.mounted) {
+          //       Navigator.pop(context);
+          //     }
+          //   },
+          //   child: const Text('Save'),
+          // ),
         ],
       )),
     );
