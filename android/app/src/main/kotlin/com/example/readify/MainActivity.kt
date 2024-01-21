f1d3d85986a46d4c1bf8847d99dd5bf9cc921950
stoprain <mystoprain@gmail.com>
2024-01-21 13:44:39 +0800

    rename

--- android/app/src/main/kotlin/com/example/readify/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/readify/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tinyreader
+package com.example.readify
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tinyreader/main.dart';
+import 'package:readify/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
